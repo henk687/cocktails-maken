56b708ed2417e43fdaf3ba66f67134e3588fba0c
henk687 <henkleenheer@upcmail.nl>
2021-06-25 17:20:59 +0200

    changes

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function isInViewport(el) {
 const counter = document.querySelector('.counter');
 
 const counters = document.querySelectorAll('.counter');
-const speed = 200; // The lower the slower
+const speed = 400; // The lower the slower
 
 document.addEventListener('scroll', function () {
     if (isInViewport(counter)) {
@@ -23,7 +23,7 @@ document.addEventListener('scroll', function () {
             const count = +counter.innerText;
     
             // Lower inc to slow and higher to slow
-            const inc = target / speed;
+            const inc = Math.trunc(target / speed);
     
             // Check if target is reached
             if (count < target) {
@@ -35,12 +35,9 @@ document.addEventListener('scroll', function () {
                 counter.innerText = target;
             }
         };
-    
         updateCount();
     })
 }});
-
-
 
 const header = document.querySelector('header');
 
@@ -96,98 +93,4 @@ function openTab(event, tabName) {
     event.currentTarget.classList.add("active")
     document.getElementById(tabName).style.transform = "translateX(0)";
     document.getElementById(tabName).style.transitionDelay = "0.3s";
-}
-
-
-// Form validation
-
-const form = document.querySelector("form");
-const thankYou = document.querySelector(".thank-you");
-const nameInput = document.querySelector('input[name="name"]');
-const emailInput = document.querySelector('input[name="email"]');
-const phoneInput = document.querySelector('input[name="phone"]');
-const messageInput = document.querySelector('textarea[name="message"]');
-
-const inputs = [nameInput, emailInput, phoneInput, messageInput]
-
-let isFormValid = false;
-let isValidationOn = false;
-
-const isValidEmail = (email) => {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-};
-  
-const isValidPhone = (phone) => {
-    const re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
-    return re.test(String(phone).toLowerCase());
-};
-
-const resetElm = (elm) => {
-    elm.classList.remove("invalid");
-    elm.nextElementSibling.classList.add("hidden");
-};
-
-const invalidateElm = (elm) => {
-    elm.classList.add("invalid")
-    elm.nextElementSibling.classList.remove("hidden");
-};
-
-const validateInputs = () => {
-    if (!isValidationOn) return;
-
-    isFormValid = true;
-    inputs.forEach(resetElm);
-
-    if (!nameInput.value) {
-        isFormValid = false;
-        invalidateElm(nameInput);
-    }
-
-    if (!isValidEmail(emailInput.value)) {
-        isFormValid = false;
-        invalidateElm(emailInput);
-    }
-
-    if (!isValidPhone(phoneInput.value)) {
-        isFormValid = false;
-        invalidateElm(phoneInput);
-    }
-
-    if (!messageInput.value) {
-        isFormValid = false;
-        invalidateElm(messageInput);
-    }
-};
-
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    isValidationOn = true;
-    validateInputs();
-    if (isFormValid) {
-        sendMail();
-        form.remove();
-        thankYou.classList.remove("hidden");
-    }
-});
-
-inputs.forEach((input) => {
-    input.addEventListener("input", () => {
-        validateInputs();
-    });
-});
-
-
-function sendMail(params) {
-    var tempParams = {
-        name:document.getElementById("name").value,
-        email:document.getElementById("email").value,
-        phone:document.getElementById("phone").value,
-        message:document.getElementById("message").value
-    };
-
-    emailjs.send('mail_cocktail', 'template_k8v4nhw', tempParams)
-    .then(function(res){
-        console.log("succes", res.status);
-    })
 };
